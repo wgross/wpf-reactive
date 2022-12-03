1812a42e0e0ffb6ae59fe27bcf1f5e5206635a57
wgross <github@text-mode.de>
2022-12-03 16:22:29 +0100

    Add project files.

---
+++ src/App.Reactive/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace App.Reactive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ src/App.Reactive/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace App.Reactive;
+
+public class MainWindowViewModel : ReactiveObject
+{
+    public MainWindowViewModel()
+    {
+        this.Command = ReactiveCommand.CreateFromTask(this.CommandExecutedAsync);
+        this.result = this.Command
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .ToProperty(this, x => x.Result);
+    }
+
+    public ReactiveCommand<Unit, string> Command { get; }
+
+    private ObservableAsPropertyHelper<string> result;
+
+    public string Result => this.result.Value;
+
+    private async Task<string> CommandExecutedAsync()
+    {
+        await Task.Delay(1000);
+
+        return DateTimeOffset.UtcNow.ToString();
+    }
+}
